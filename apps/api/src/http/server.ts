223e671dc8a8ae8e84d4fc930f5aceafbdda5a3f
andreenakashima <andreenakashima@gmail.com>
2024-06-21 10:24:17 -0300

    route user profile authenticated

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -11,6 +11,7 @@ import {
 import { createAccount } from "./routes/auth/create-account";
 import { authenticateWithPassword } from "./routes/auth/authenticate-with-password";
 import fastifyJwt from "@fastify/jwt";
+import { getProfile } from "./routes/auth/get-profile";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -40,6 +41,7 @@ app.register(fastifyCors);
 
 app.register(createAccount);
 app.register(authenticateWithPassword);
+app.register(getProfile);
 
 app.listen({ port: 3333 }).then(() => {
 	console.log("http server running");
