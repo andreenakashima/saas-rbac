1f9ed267f0b485fb8c46d52cfe155740c9a3e9c8
andreenakashima <andreenakashima@gmail.com>
2024-06-25 10:28:13 -0300

    get project details route

---
+++ apps/api/src/http/routes/projects/get-project.ts
@@ -0,0 +1,89 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { auth } from "../../middlewares/auth";
+import z from "zod";
+import { getUserPermissions } from "../../../utils/get-user-permissions";
+import { UnauthorizedError } from "../_errors/unauthorized-error";
+import { prisma } from "../../../lib/prisma";
+import { BadRequestError } from "../_errors/bad-request-error";
+
+export async function getProject(app: FastifyInstance) {
+	app
+		.withTypeProvider<ZodTypeProvider>()
+		.register(auth)
+		.get(
+			"/organizations/:orgSlug/projects/:projectSlug",
+			{
+				schema: {
+					tags: ["projects"],
+					summary: "Get project details",
+					security: [{ bearerAuth: [] }],
+					params: z.object({
+						orgSlug: z.string(),
+						projectSlug: z.string(),
+					}),
+					response: {
+						200: z.object({
+							project: z.object({
+								description: z.string(),
+								id: z.string().uuid(),
+								name: z.string(),
+								slug: z.string(),
+								avatarUrl: z.string().nullable(),
+								organizationId: z.string().uuid(),
+								ownerId: z.string(),
+								owner: z.object({
+									id: z.string().uuid(),
+									name: z.string().nullable(),
+									avatarUrl: z.string().nullable(),
+								}),
+							}),
+						}),
+					},
+				},
+			},
+			async (request, reply) => {
+				const { orgSlug, projectSlug } = request.params;
+				const userId = await request.getCurrentUserId();
+				const { organization, membership } =
+					await request.getUserMembership(orgSlug);
+
+				const { cannot } = getUserPermissions(userId, membership.role);
+
+				if (cannot("get", "Project")) {
+					throw new UnauthorizedError(
+						"You are not allowed to see this project"
+					);
+				}
+
+				const project = await prisma.project.findUnique({
+					select: {
+						id: true,
+						name: true,
+						description: true,
+						slug: true,
+						ownerId: true,
+						avatarUrl: true,
+						organizationId: true,
+						owner: {
+							select: {
+								id: true,
+								name: true,
+								avatarUrl: true,
+							},
+						},
+					},
+					where: {
+						slug: projectSlug,
+						organizationId: organization.id,
+					},
+				});
+
+				if (!project) {
+					throw new BadRequestError("Project not found");
+				}
+
+				return reply.send({ project });
+			}
+		);
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -26,6 +26,7 @@ import { shutdownOrganization } from "./routes/orgs/shutdown-organization";
 import { transferOrganization } from "./routes/orgs/transfer-organization";
 import { createProject } from "./routes/projects/create-project";
 import { deleteProject } from "./routes/projects/delete-project";
+import { getProject } from "./routes/projects/get-project";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -79,6 +80,7 @@ app.register(transferOrganization);
 
 app.register(createProject);
 app.register(deleteProject);
+app.register(getProject);
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
 	console.log("http server running");
