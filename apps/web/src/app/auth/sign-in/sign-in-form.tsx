a627cd81259ab65a63602fa4bda7dbb7b9a993bd
andreenakashima <andreenakashima@gmail.com>
2024-07-01 09:43:05 -0300

    authenticate with github

--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -12,6 +12,7 @@ import { AlertTriangle, Loader2 } from "lucide-react";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { useFormState } from "@/hooks/use-form-state";
 import { useRouter } from "next/navigation";
+import { signInWithGithub } from "../actions";
 
 export function SignInForm() {
 	const router = useRouter();
@@ -24,64 +25,68 @@ export function SignInForm() {
 	);
 
 	return (
-		<form onSubmit={handleSubmit} className="space-y-4">
-			{success === false && message && (
-				<Alert variant="destructive">
-					<AlertTriangle className="size-4" />
-					<AlertTitle>Sign in failed!</AlertTitle>
-					<AlertDescription>
-						<p>{message}</p>
-					</AlertDescription>
-				</Alert>
-			)}
-
-			<div className="space-y-1">
-				<Label htmlFor="email">E-mail</Label>
-				<Input name="email" id="email" type="email" />
-
-				{errors?.email && (
-					<p className="text-xs font-medium text-red-500 dark:text-red-400">
-						{errors.email[0]}
-					</p>
-				)}
-			</div>
-
-			<div className="space-y-1">
-				<Label htmlFor="password">Password</Label>
-				<Input name="password" id="password" type="password" />
-
-				{errors?.password && (
-					<p className="text-xs font-medium text-red-500 dark:text-red-400">
-						{errors.password[0]}
-					</p>
+		<div className="space-y-4">
+			<form onSubmit={handleSubmit} className="space-y-4">
+				{success === false && message && (
+					<Alert variant="destructive">
+						<AlertTriangle className="size-4" />
+						<AlertTitle>Sign in failed!</AlertTitle>
+						<AlertDescription>
+							<p>{message}</p>
+						</AlertDescription>
+					</Alert>
 				)}
 
-				<Link
-					href="/auth/forgot-password"
-					className="text-xs font-medium text-foreground hover:underline"
-				>
-					Forgot your password?
-				</Link>
-			</div>
+				<div className="space-y-1">
+					<Label htmlFor="email">E-mail</Label>
+					<Input name="email" id="email" type="email" />
 
-			<Button type="submit" className="w-full">
-				{isPending ? (
-					<Loader2 className="size-4 animate-spin" />
-				) : (
-					"Sign in with e-mail"
-				)}
-			</Button>
+					{errors?.email && (
+						<p className="text-xs font-medium text-red-500 dark:text-red-400">
+							{errors.email[0]}
+						</p>
+					)}
+				</div>
 
-			<Button variant="link" className="w-full" size="sm" asChild>
-				<Link href="/auth/sign-up">Create a new account</Link>
-			</Button>
+				<div className="space-y-1">
+					<Label htmlFor="password">Password</Label>
+					<Input name="password" id="password" type="password" />
+
+					{errors?.password && (
+						<p className="text-xs font-medium text-red-500 dark:text-red-400">
+							{errors.password[0]}
+						</p>
+					)}
+
+					<Link
+						href="/auth/forgot-password"
+						className="text-xs font-medium text-foreground hover:underline"
+					>
+						Forgot your password?
+					</Link>
+				</div>
+
+				<Button type="submit" className="w-full">
+					{isPending ? (
+						<Loader2 className="size-4 animate-spin" />
+					) : (
+						"Sign in with e-mail"
+					)}
+				</Button>
+
+				<Button variant="link" className="w-full" size="sm" asChild>
+					<Link href="/auth/sign-up">Create a new account</Link>
+				</Button>
+			</form>
 
 			<Separator />
 
-			<Button type="submit" variant="outline" className="w-full">
-				<Image src={githubIcon} className="size-4 mr-2 dark:invert" alt="" />
-				Sign in with Github
-			</Button>
-		</form>
+			<form action={signInWithGithub}>
+				<Button type="submit" variant="outline" className="w-full">
+					<Image src={githubIcon} className="size-4 mr-2 dark:invert" alt="" />
+					Sign in with Github
+				</Button>
+			</form>
+		</div>
 	);
 }
