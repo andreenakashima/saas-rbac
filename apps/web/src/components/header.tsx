aee2ab734fbca05ee907363243609dca9261fe65
andreenakashima <andreenakashima@gmail.com>
2024-07-02 14:34:25 -0300

    user permissions

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -3,8 +3,11 @@ import Image from "next/image";
 import ProfileButton from "./profile-button";
 import { Slash } from "lucide-react";
 import { OrganizationSwitcher } from "./organization-switcher";
+import { ability } from "@/auth/auth";
 
 export async function Header() {
+	const permissions = await ability();
+
 	return (
 		<div className="mx-auto flex max-w-[1200px] items-center justify-between">
 			<div className="flex items-center gap-3">
@@ -13,6 +16,8 @@ export async function Header() {
 				<Slash className="size-3 -rotate-[24deg] text-border" />
 
 				<OrganizationSwitcher />
+
+				{permissions?.can("get", "Project") && <p>Projetos</p>}
 			</div>
 
 			<div className="flex items-center gap-4">
