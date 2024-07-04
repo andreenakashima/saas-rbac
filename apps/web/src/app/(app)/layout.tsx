1cee32779d51b6727d33ccd4f1c43ecb3ef61fb7
andreenakashima <andreenakashima@gmail.com>
2024-07-04 15:36:42 -0300

    Parallel and interception routes

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,19 +1,19 @@
 import { isAuthenticated } from "@/auth/auth";
-import { Header } from "@/components/header";
 import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function AppLayout({
 	children,
-}: Readonly<{ children: React.ReactNode }>) {
+	sheet,
+}: Readonly<{ children: React.ReactNode; sheet: React.ReactNode }>) {
 	if (!isAuthenticated()) {
 		redirect("/auth/sign-in");
 	}
 
 	return (
-		<div className="py-4 space-y-4">
-			<Header />
-			<main className="mx-auto w-full max-w-[1200px]">{children}</main>
-		</div>
+		<>
+			{children}
+			{sheet}
+		</>
 	);
 }
