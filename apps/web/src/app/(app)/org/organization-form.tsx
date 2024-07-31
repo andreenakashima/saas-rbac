9719a031303613b0df18ae523706ad053feecdad
andreenakashima <andreenakashima@gmail.com>
2024-07-31 10:01:59 -0300

    organization settings page

--- apps/web/src/app/(app)/org/organization-form.tsx
+++ apps/web/src/app/(app)/org/organization-form.tsx
@@ -7,7 +7,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useFormState } from "@/hooks/use-form-state";
 import { AlertTriangle, Loader2 } from "lucide-react";
-import { createOrganizationAction } from "./actions";
+import { createOrganizationAction } from "../create-organization/actions";
 
 export function OrganizationForm() {
 	const [{ success, message, errors }, handleSubmit, isPending] = useFormState(
