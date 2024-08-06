b91f1792e370d96a2cb2414b402812273e081583
andreenakashima <andreenakashima@gmail.com>
2024-08-06 14:20:31 -0300

    accept invite action

--- apps/web/src/app/auth/sign-in/actions.ts
+++ apps/web/src/app/auth/sign-in/actions.ts
@@ -4,6 +4,7 @@ import { signInWithPassword } from "@/http/sign-in-with-password";
 import { HTTPError } from "ky";
 import { z } from "zod";
 import { cookies } from "next/headers";
+import { acceptInvite } from "@/http/accept-invite";
 
 const signInSchema = z.object({
 	email: z.string().email({ message: "Please provide a valid email address." }),
@@ -31,6 +32,15 @@ export async function signInWithEmailAndPassword(data: FormData) {
 			path: "/",
 			maxAge: 60 * 60 * 24 * 7, // 7 days
 		});
+
+		const inviteId = cookies().get("invitId")?.value;
+
+		if (inviteId) {
+			try {
+				await acceptInvite(inviteId);
+				cookies().delete("inviteId");
+			} catch {}
+		}
 	} catch (err) {
 		if (err instanceof HTTPError) {
 			const { message } = await err.response.json();
--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -11,11 +11,12 @@ import githubIcon from "@/assets/github-icon.svg";
 import { AlertTriangle, Loader2 } from "lucide-react";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { useFormState } from "@/hooks/use-form-state";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { signInWithGithub } from "../actions";
 
 export function SignInForm() {
 	const router = useRouter();
+	const searchParams = useSearchParams();
 
 	const [{ success, message, errors }, handleSubmit, isPending] = useFormState(
 		signInWithEmailAndPassword,
@@ -39,7 +40,12 @@ export function SignInForm() {
 
 				<div className="space-y-1">
 					<Label htmlFor="email">E-mail</Label>
-					<Input name="email" id="email" type="email" />
+					<Input
+						name="email"
+						id="email"
+						type="email"
+						defaultValue={searchParams.get("email") ?? ""}
+					/>
 
 					{errors?.email && (
 						<p className="text-xs font-medium text-red-500 dark:text-red-400">
