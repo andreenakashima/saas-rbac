b0311af33dc939efce86359e782f0f2a12db50a3
andreenakashima <andreenakashima@gmail.com>
2024-08-06 11:04:16 -0300

    project list

--- apps/web/src/app/(app)/org/[slug]/(projects)/project-list.tsx
+++ apps/web/src/app/(app)/org/[slug]/(projects)/project-list.tsx
@@ -1,3 +1,4 @@
+import { getCurrentOrg } from "@/auth/auth";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
@@ -7,34 +8,53 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import { getProjects } from "@/http/get-projects";
 import { ArrowRight } from "lucide-react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
-export function ProjectList() {
+dayjs.extend(relativeTime);
+
+export async function ProjectList() {
+	const currentOrg = getCurrentOrg();
+	const { projects } = await getProjects(currentOrg!);
+
 	return (
 		<div className="grid grid-cols-3 gap-4">
-			<Card>
-				<CardHeader>
-					<CardTitle></CardTitle>
-					<CardDescription className="line-clamp-2 leading-relaxed"></CardDescription>
-				</CardHeader>
+			{projects.map((project) => {
+				return (
+					<Card key={project.id} className="flex flex-col justify-between">
+						<CardHeader>
+							<CardTitle className="text-xl font-medium">
+								{project.name}
+							</CardTitle>
+							<CardDescription className="line-clamp-2 leading-relaxed">
+								{project.description}
+							</CardDescription>
+						</CardHeader>
 
-				<CardFooter className="flex items-center gap-1.5">
-					<Avatar className="size-4">
-						<AvatarImage src="" />
-						<AvatarFallback />
-					</Avatar>
+						<CardFooter className="flex items-center gap-1.5">
+							<Avatar className="size-4">
+								{project.owner.avatarUrl && (
+									<AvatarImage src={project.owner.avatarUrl} />
+								)}
+								<AvatarFallback />
+							</Avatar>
 
-					<span className="text-xs text-muted-foreground">
-						Created by{" "}
-						<span className="font-medium text-foreground">Andre Nakashima</span>{" "}
-						a day ago
-					</span>
+							<span className="text-xs text-muted-foreground truncate">
+								<span className="font-medium text-foreground">
+									{project.owner.name}
+								</span>{" "}
+								{dayjs(project.createdAt).fromNow()}
+							</span>
 
-					<Button size="xs" variant="outline" className="ml-auto">
-						View <ArrowRight className="size-3 ml-2" />
-					</Button>
-				</CardFooter>
-			</Card>
+							<Button size="xs" variant="outline" className="ml-auto">
+								View <ArrowRight className="size-3 ml-2" />
+							</Button>
+						</CardFooter>
+					</Card>
+				);
+			})}
 		</div>
 	);
 }
