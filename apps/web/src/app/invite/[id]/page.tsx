c57e3d91c2a141dca8d0acf07eafb7a6af98d795
andreenakashima <andreenakashima@gmail.com>
2024-08-07 09:09:07 -0300

    invite with another email

--- apps/web/src/app/invite/[id]/page.tsx
+++ apps/web/src/app/invite/[id]/page.tsx
@@ -6,8 +6,9 @@ import { acceptInvite } from "@/http/accept-invite";
 import { getInvite } from "@/http/get-invite";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { CheckCircle, LogIn } from "lucide-react";
+import { CheckCircle, LogIn, LogOut } from "lucide-react";
 import { cookies } from "next/headers";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 dayjs.extend(relativeTime);
@@ -90,6 +91,34 @@ export default async function InvitePage({ params }: InvitePageProps) {
 							Join {invite.organization.name}
 						</Button>
 					</form>
+				)}
+
+				{isUserAuthenticated && !userIsAuthenticatedWithSameEmailFromInvite && (
+					<div className="space-y-4">
+						<p className="text-balance text-center text-sm leading-relaxed text-muted-foreground">
+							This invite was sent to{" "}
+							<span className="text-foreground font-medium">
+								{invite.email}
+							</span>{" "}
+							but you are currently authenticated as{" "}
+							<span className="text-foreground font-medium">
+								{currentUserEmail}
+							</span>
+						</p>
+
+						<div className="space-y-2">
+							<Button className="w-full" variant="secondary" asChild>
+								<a href="/api/auth/sign-out">
+									<LogOut className="size-4 mr-2" />
+									Sign out from {currentUserEmail}
+								</a>
+							</Button>
+
+							<Button className="w-full" variant="secondary" asChild>
+								<Link href="/api/auth/sign-out">Back to dashboard</Link>
+							</Button>
+						</div>
+					</div>
 				)}
 			</div>
 		</div>
