3c515def7c6fe64439b2eb886893d02e7c155332
andreenakashima <andreenakashima@gmail.com>
2024-08-07 09:27:38 -0300

    pending invites structure

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -7,6 +7,7 @@ import { ability } from "@/auth/auth";
 import { Separator } from "./ui/separator";
 import { ThemeSwitcher } from "./theme/theme-switcher";
 import { ProjectSwitcher } from "./project-switcher";
+import { PendingInvites } from "./pending-invites";
 
 export async function Header() {
 	const permissions = await ability();
@@ -29,6 +30,7 @@ export async function Header() {
 			</div>
 
 			<div className="flex items-center gap-4">
+				<PendingInvites />
 				<ThemeSwitcher />
 				<Separator orientation="vertical" className="h-5" />
 				<ProfileButton />
